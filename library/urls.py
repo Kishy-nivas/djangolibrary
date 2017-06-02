cbd320fa75c0b3ee310495d0473f8557bd95b898
Kishy-nivas <kishynivas10@gmail.com>
2017-06-02 14:17:58 +0530

    Correctly setted up models till now

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from .import views
 
-url patterns =[
-    url('^$',view.index, name ='index'),  # matching with an empty string 
+urlpatterns =[
+    url('^$',views.index, name ='index'),  # matching with an empty string 
+    
 
 ]
